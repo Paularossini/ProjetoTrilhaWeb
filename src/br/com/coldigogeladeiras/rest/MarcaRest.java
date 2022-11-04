7eee5102c3f8094f93485800c4928bb1a44cc672
paula.moura <paularossini96@gmail.com>
2022-11-03 22:03:46 -0300

    Atualizcao Cria objeto Marca

--- src/br/com/coldigogeladeiras/rest/MarcaRest.java
+++ src/br/com/coldigogeladeiras/rest/MarcaRest.java
@@ -1,10 +1,16 @@
 package br.com.coldigogeladeiras.rest;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import br.com.coldigogeladeiras.modelo.Marca;
+import br.com.coldigogeladiras.bd.Conexao;
 
 @Path("marca")
 public class MarcaRest {
@@ -13,6 +19,9 @@ public class MarcaRest {
 	@Path("/buscar")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response buscar() {
+		List<Marca>listaMarcas = new ArrayList<Marca>();
 		
+		Conexao conec = new Conexao();
+		Connection conexao = conec.abrirConexao();
 	}
 }
